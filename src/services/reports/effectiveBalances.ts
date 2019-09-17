4dc27b10b01479f2a636dd4dcb27b19f6b6728c3
Anja Ishmukhametova <miukki@gmail.com>
2019-09-17 23:35:26 +1000

    updated service

--- src/services/reports/effectiveBalances.ts
+++ src/services/reports/effectiveBalances.ts
@@ -2,13 +2,13 @@ import { Container, Service } from 'typedi';
 
 import { environment } from '../../environments/environment';
 import { LoggerService } from '../logger.service';
-import { BalanceController } from '../../controllers/balance';
 import { AccountsBalanceHistory, IEffectiveBalance } from '../../models/balances';
 import { ClientSession, BulkWriteOpResultObject } from 'mongodb';
 import { Account, AccountType } from '../../models/accounts';
+import { User } from '../../models/users';
+import { Currency, ICurrency } from '../../models/currencies';
 import { Types } from 'mongoose';
 import moment from 'moment';
-import { threadId } from 'worker_threads';
 
 const {
   performance,
@@ -18,10 +18,9 @@ const {
 @Service()
 export class EffectiveBalance {
   private logger: LoggerService = Container.get(LoggerService);
-  // private balanceController: BalanceController = Container.get(BalanceController);
 
   constructor() {
-   this.demon(24 * 60 * 60 * 1000);
+    this.demon(24 * 60 * 60 * 1000);
   }
 
   get startDate () {
@@ -34,15 +33,17 @@ export class EffectiveBalance {
     this.logger.info(`Process is started`);
     const effectiveDates = await this.getEffectiveDates();
     // TODO: kak dobavlyaetsya accountsID?
-    return await this.makeReport({dates: effectiveDates, ids: ['5be92db0e6687d38b2d33817']});
+    // tslint:disable-next-line:max-line-length
+    return await this.makeReport({dates: effectiveDates, ids: []});
   }
+
   async demon(n, isNoError = true) {
     while (isNoError) {
       console.log(`Start proccess`);
       try {
         const result = await this.bodyAsyncForDemon();
-        this.logger.info(`Success, next recalculation in ${n} mills`, { result: JSON.stringify(result) });
-        await delay(n);
+        this.logger.info(`Success, next recalculation in ${n / 1000 / 60 / 60} h`, { result: JSON.stringify(result) });
+        await this.delay(n);
       } catch (error) {
         isNoError = false;
         this.logger.error(`error`, { error });
@@ -61,6 +62,169 @@ export class EffectiveBalance {
       latestDateInCollection = latestDateInCollection.map(({effectiveDate}) => this.formatDate(effectiveDate));
       return this.countEffectiveDates(new Date(latestDateInCollection), new Date());
     }
+
+  }
+
+
+  async getCurrencies (): Promise<ICurrency[]> {
+    const currencies: Array<ICurrency> = await Currency.find({}, null, { lean: true });
+    return currencies;
+  }
+
+  async getEffectiveBalancesFromCollection ({ids = [], effectiveDate = null}): Promise<any[]> {
+
+    const currencies = (await this.getCurrencies()).map(i => i.currency);
+
+    const currQueriesFirst = currencies.reduce((acc, curr) => {
+      acc[curr] = {
+        '$cond' : {
+          'if' : {
+              '$eq' : [
+                  '$_id.currency',
+                  curr
+              ]
+          },
+          'then' : '$totalEffectiveBalance',
+          'else' : 0.0
+          }
+        };
+      return acc;
+    }, {});
+
+    const currQueriesSecond = currencies.reduce((acc, curr) => {
+      return {
+        ...acc,
+        [curr]: {'$sum' : '$' + curr},
+      };
+    }, {});
+
+    const aggregationQueryAccountsBalanceistory = [
+    {
+      '$group' : {
+        '_id' : {
+            'type' : '$type',
+            'currency' : '$currency'
+        },
+        'totalEffectiveBalance' : {
+            '$sum' : '$effectiveBalance'
+        },
+        'type' : {
+            '$first' : '$type'
+        },
+        'currency' : {
+            '$first' : '$currency'
+        }
+      }
+    },
+    {
+      '$lookup' : {
+        'from' : 'dic_accountancy',
+        'localField' : '_id.type',
+        'foreignField' : 'code',
+        'as' : 'description'
+      }
+    },
+    {
+      '$addFields' : {
+        'description' : {
+            '$arrayElemAt' : [
+                '$description',
+                0.0
+            ]
+        }
+      }
+    },
+    {
+      '$addFields' : {
+        'type' : '$description.type',
+        'name' : '$description.name'
+      }
+    },
+    {
+      '$project': {
+        '_id' : 1,
+        'type' : 1,
+        'name' : 1,
+        'currency': 1,
+        'totalEffectiveBalance' : {
+            '$cond' : {
+                'if' : {
+                    '$eq' : [
+                        '$type',
+                        'debit'
+                    ]
+                },
+                'then' : {
+                    '$multiply' : [
+                        '$totalEffectiveBalance',
+                        -1
+                    ]
+                },
+                'else' : '$totalEffectiveBalance'
+            }
+         }
+      }
+    },
+    {
+      '$project': {
+        '_id' : 1,
+        'type' : 1,
+        'name' : 1,
+        ...currQueriesFirst
+      }
+    },
+    {
+      '$facet': {
+        'accounts' : [
+          {
+              '$group' : {
+                  '_id' : '$_id.type',
+                  'name' : {
+                      '$first' : '$name'
+                  },
+                  'type' : {
+                      '$first' : '$type'
+                  },
+                  ...currQueriesSecond
+              }
+          },
+          {
+            '$sort': {
+              'type' : -1,
+              '_id' : 1
+            }
+          }
+        ],
+        'totals' : [
+          {
+              '$group' : {
+                  '_id' : '$type',
+                  ...currQueriesSecond
+              }
+          },
+          {
+              '$sort' : {
+                  '_id' : -1
+              }
+          }
+        ]
+
+      }
+    },
+  ];
+  const query = [
+    {
+      $match: {
+        accountId: {'$in': [...ids.map(id => id + '')]},
+        effectiveDate: moment(effectiveDate + 'T00:00:00.000Z').toDate()
+      }
+    },
+    ...aggregationQueryAccountsBalanceistory
+  ];
+
+  const result = (await AccountsBalanceHistory.aggregate(query))
+    .reduce((acc, i) => { acc = {...acc, ...i}; return acc; } , {});
+  return result;
 
   }
 
@@ -83,22 +247,26 @@ export class EffectiveBalance {
   formatDate (d) {
     return moment(new Date(d)).format('YYYY-MM-DD');
   }
-  private countEffectiveDates(startDate, stopDate) {
-
+  public countEffectiveDates(startDate, stopDate) {
     let dateArray = new Array();
     let currentDate = startDate;
     while (currentDate <= stopDate) {
         dateArray = [...dateArray, this.formatDate(currentDate)];
         currentDate = new Date(currentDate).setDate(new Date(currentDate).getDate() + 1);
     }
-    console.log('effectiveDates', dateArray);
     return dateArray;
 
   }
 
 
   async makeReport({ dates, ids }) {
-    return this.fireEffectiveBalance({dates, ids});
+    return this.fireRecalcEffectiveBalance({dates, ids});
+  }
+
+  async getEffectiveBalances (effectiveDate) {
+    const brokerAccountsID = await this.getBrockerAccountsId();
+    const result = await this.getEffectiveBalancesFromCollection({ids: brokerAccountsID, effectiveDate});
+    return result;
   }
 
   async inserMany (
@@ -124,13 +292,14 @@ export class EffectiveBalance {
     return balances
     .map(
         balance => {
-          const {currency, accountId, effectiveDate, totalFromFiltered, totalToFiltered, effectiveBalance } = balance;
+          const {currency, accountId, effectiveDate, totalFromFiltered, totalToFiltered, effectiveBalance, type } = balance;
           return ({
             updateOne: {
               filter: { currency, accountId, effectiveDate },
               update: {
-                $set: { updatedAt: timestamp, totalFromFiltered, totalToFiltered, effectiveBalance }
+                $set: { updatedAt: timestamp, totalFromFiltered, totalToFiltered, effectiveBalance, type }
               },
+              upsert: true,
             }
           });
         }
@@ -144,7 +313,9 @@ export class EffectiveBalance {
   ): Promise<BulkWriteOpResultObject> {
     try {
       const options = { ordered: false, session };
-      const output = await AccountsBalanceHistory.collection.bulkWrite(this.balancesUpdate(balances), options);
+      const output = balances.length ? await AccountsBalanceHistory.collection.bulkWrite(this.balancesUpdate(balances), options) : {};
+      const { result= {} } = output;
+      result['data'] = balances;
       console.log('SUCCESS bulkWrite', output);
       return output;
     } catch (error) {
@@ -160,6 +331,7 @@ export class EffectiveBalance {
       aggregationQuery => (
         Account.aggregate(ids.length === 0 ?
           [
+            {$match: { }},
             ...aggregationQuery
           ] :
           [
@@ -171,10 +343,28 @@ export class EffectiveBalance {
     )).reduce((acc, val) => acc.concat(val), []);
   }
 
-  async fireEffectiveBalance ({dates, ids }): Promise<any> {
+  async getBrockerAccountsId(): Promise<any[]> {
+    return ((await User.aggregate(
+      [
+        {
+          $match: {
+            type: 'broker',
+          }
+        },
+        {
+          $project: {
+            accounts: 1,
+            type: 1,
+          }
+        }
+      ]
+    )).map(_id => _id.accounts)).reduce((acc, val) => acc.concat(val), []);
+  }
+
+
+  async fireRecalcEffectiveBalance ({dates, ids }): Promise<any> {
 
     const effectiveDates = dates.map(date => new Date(date));
-    console.log('effectiveDates', effectiveDates);
     const before = performance.now();
 
     const aggregationQueries = effectiveDates.map(effectiveDate => [
@@ -272,49 +462,16 @@ export class EffectiveBalance {
 
     const after = performance.now();
     const mills = (after - before);
-
-    console.log('effectiveBalances', effectiveBalances, 'mills', mills);
-
     const count = await AccountsBalanceHistory.find().countDocuments();
+
     let result;
     if (count === 0) {
       result = await this.inserMany(effectiveBalances);
     } else {
       result = await this.bulkWrite(effectiveBalances);
-    // todo: voswrat dannih seichas prohodit
-    //   "data": {
-    //     "ok": 1,
-    //     "writeErrors": [],
-    //     "writeConcernErrors": [],
-    //     "insertedIds": [],
-    //     "nInserted": 0,
-    //     "nUpserted": 0,
-    //     "nMatched": 3,
-    //     "nModified": 3,
-    //     "nRemoved": 0,
-    //     "upserted": [],
-    //     "lastOp": {
-    //         "ts": "6732412277871345667",
-    //         "t": 7
-    //     }
-    // }
     }
 
     return result;
   }
-
-
-  //   setTimeout(async () => {
-  //     await this.rates.getNewRatesAndSave();
-  //     this.updateRates();
-  //     const arg = `Node.js`;
-  //     // setTimeout((arg)=>console.log(arg), 2 * 1000, `${arg}`);
-  //     const tick = (n, errorCount) => {
-  //       console.log(`init every ${n} mills `)
-  //       setTimeout(d => tick(d, errorCount), n * 1000, n*2);
-  //     }
-  //     tick(24*60*60, 0)//h/min/sec
-
-  // }, environment.rateTime * 1000);
 
 }
